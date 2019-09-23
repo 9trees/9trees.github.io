137474b20e47ce4e2fa2544bdb0cb6604a5e817a
aidsg <shivv@aidrivers.ai>
2019-09-23 16:01:35 +0800

    Update_11

--- img_collage/path2json.py
+++ img_collage/path2json.py
@@ -4,19 +4,23 @@ import json
 path = './SImg' 
 data = {}
 data['fpath'] = []
+i = 0
 
 # r=root, d=directories, f = files 
 for r, d, f in os.walk(path):
 	for file in f:
-		print(os.path.join(r, file)[2:])
+		nfname = str(i) + '.' + file.split('.')[-1:][0]
+		os.rename(os.path.join(r, file), os.path.join(r, nfname))
+		print(os.path.join(r, nfname)[2:])
 		data['fpath'].append({
-			'fname': file,
-			'path': os.path.join(r, file)[2:]
+			'fname': nfname,
+			'path': os.path.join(r, nfname)[2:]
 			})
+		i += 1
 
 
 
-filename = "img.json"
+filename = "nimg.json"
 
 # If the file name exists, write a JSON string into the file.
 if filename:
