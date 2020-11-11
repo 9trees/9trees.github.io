cbb3aa12c096a7e680df05ecf7c52e3e54fba697
9trees <sivashankaryadav@outlook.com>
2020-11-11 16:56:32 +0000

    new gallery.html

--- img_collage/htmlTag.py
+++ img_collage/htmlTag.py
@@ -2,7 +2,12 @@ import os
 
 path = './SImg' 
 
+# for r, d, f in os.walk(path):
+# 	for file in f:
+# 		tag = '<a href="'+os.path.join(r, file)+'"><img src="'+os.path.join(r, file)+'"></img></a>'
+# 		print(tag)
+
 for r, d, f in os.walk(path):
 	for file in f:
-		tag = '<a href="'+os.path.join(r, file)+'"><img src="'+os.path.join(r, file)+'"></img></a>'
+		tag = '<div class="col-sm-6 col-md-4 col-lg-3 item"><a href="'+os.path.join(r, file)+'"data-lightbox="photos"><img class="img-fluid" src="'+os.path.join(r, file)+'"></img></a></div>'
 		print(tag)
