0b8f56f1d037e34ab757325e08724408827e7551
9trees <sivashankaryadav@outlook.com>
2020-11-11 14:59:45 +0000

    Wedding Photos

--- img_collage/path2json.py
+++ img_collage/path2json.py
@@ -1,7 +1,8 @@
 import os
 import json
 
-path = './SImg' 
+path = './SImg'
+# path = os.path.join(os.getcwd(), path)
 data = {}
 data['fpath'] = []
 i = 0
@@ -9,18 +10,18 @@ i = 0
 # r=root, d=directories, f = files 
 for r, d, f in os.walk(path):
 	for file in f:
-		nfname = str(i) + '.' + file.split('.')[-1:][0]
-		os.rename(os.path.join(r, file), os.path.join(r, nfname))
-		print(os.path.join(r, nfname)[2:])
+		newFileName = "new_" + str(i) + '.' + file.split('.')[-1:][0]
+		os.rename(os.path.join(r, file), os.path.join(r, newFileName))
+		print(os.path.join(r, newFileName)[2:])
 		data['fpath'].append({
-			'fname': nfname,
-			'path': os.path.join(r, nfname)[2:]
+			'fname': newFileName,
+			'path': os.path.join(r, newFileName)[2:]
 			})
 		i += 1
 
 
 
-filename = "nimg.json"
+filename = "newimg.json"
 
 # If the file name exists, write a JSON string into the file.
 if filename:
